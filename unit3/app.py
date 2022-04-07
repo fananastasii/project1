ab197e3fd39aeed0dfa6ec011afc403da72117d8
fananastasii <98755974+fananastasii@users.noreply.github.com>
2022-04-07 13:43:06 +0300

    Add files via upload

---
+++ unit3/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def start():
+    title = "Play with me"
+    
+    text = """It is a place where your dreams come true"""
+
+    choices = [
+        ('play',"Play"),
+        ('run_away',"Go away")
+    ]
+
+    return render_template('adventure.html', title=title, text=text, choices=choices)
+
+
+
+@app.route("/game")
+def play():
+    title = "You chose to play"
+    
+    text = """Feel free to choose one of my magic boxes"""
+
+    choices = [
+        ('firstbox',"Choose box #1"),
+        ('secondbox',"Choose box #2")
+        ('run_away', "Go away")
+    ]
+
+    return render_template('adventure.html', title=title, text=text, choices=choices)
+
+@app.route("/escape")
+def run_away():
+    title = "You run away!"
+    
+    text = """You chose not to try out your luck!"""
+
+    choices = []
+
+    return render_template('adventure.html', title=title, text=text, choices=choices)
+
+
+
+@app.route("/box1")
+def firstbox():
+    title = "You lost!"
+    
+    text = """This box is empty. Try out your luck somewhere else"""
+
+    choices = []
+
+    return render_template('adventure.html', title=title, text=text, choices=choices)
+
+@app.route("/box2")
+def secondbox():
+    title = "You won!"
+    
+    text = """Your prize is hidden, try to find it"""
+
+    choices = []
+
+    return render_template('adventure.html', title=title, text=text, choices=choices)
